17e7fce14dfb55abae3de53a373d6b4573c77b2e
Otabek Toshtemirov <47772571+OtabekToshtemirov@users.noreply.github.com>
2023-06-01 21:07:49 +0500

    Update main.cpp
    BEM

--- main.cpp
+++ main.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 int main(int argc, char** argv) {
     double a,b,c,x,r,t;
-    cout<<"A=";
-    cin>>a;
-    cout<<"B=";
-    cin>>b;
-    cout<<"C=";
-    cin>>c;
-    cout<<"X=";
-    cin>>x;
-    cout<<"T=";
-    cin>>t;
-    if(x>1 and x<=2){
-        r= pow(x,3)*a+x*x*b+c;
+    cout << "A=";
+    cin >> a;
+    cout << "B=";
+    cin >> b;
+    cout << "C=";
+    cin >> c;
+    cout << "X=";
+    cin >> x;
+    cout << "T=";
+    cin >> t;
+    if( x > 1 and x <= 2){
+        r = pow(x,3)*a+x*x*b+c;
 
     }
-    if(x>2 and x<=3){
+    if( x > 2 and  x <= 3){
         r=a/x*x+b*x+c/ pow(x,1/2);
     }
     if(x>3){
         r= pow((a+b+c),t);
     }
-    cout<<"R="<<r;
+    cout << "R=" << r;
 
     return 0;
 }
